772bd12ea3a9c619cbc65c3adc810ded9da33c4a
XDPXI <xdpxigaming@gmail.com>
2025-09-15 18:52:13 +1000

    improve font loading #25

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import {bangs} from "./bang";
 import "./global.css";
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-500.css";
+import "@fontsource/inter/latin-600.css";
+import "@fontsource/inter/latin-700.css";
 
 // Default search engines for when no bang is used
 const DEFAULT_SEARCH_ENGINES = [
